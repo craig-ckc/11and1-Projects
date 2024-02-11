2bde11ddf4edeaa5c564b5b3264925de62492e85
Craig Chihururu <46131588+craig-ckc@users.noreply.github.com>
2024-02-11 15:09:25 +0200

    script-update

--- script.js
+++ script.js
@@ -11,158 +11,163 @@ window.addEventListener("resize", function () {
   setTrackHeights();
 });
 
-const lenis = new Lenis()
+// const lenis = new Lenis()
 
-lenis.on('scroll', ScrollTrigger.update)
+// lenis.on('scroll', ScrollTrigger.update)
 
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 1000)
-})
+// gsap.ticker.add((time) => {
+//   lenis.raf(time * 1000)
+// })
 
-gsap.ticker.lagSmoothing(0)
+// gsap.ticker.lagSmoothing(0)
 
 // Horizontal scroll
-let tlMain = gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".section-height",
-      start: "top top",
-      end: "98% bottom",
-      scrub: 1
-    }
-  })
-  .to(".section-track", {
-    xPercent: -100,
-    ease: "none"
-  });
-  
+function trustAndTransition() {
+  let tlMain = gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".section-height",
+        start: "top top",
+        end: "98% bottom",
+        scrub: 1
+      }
+    })
+    .to(".section-track", {
+      xPercent: -100,
+      ease: "none"
+    });
+
   // Hero image
   gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".empty-panel",
-      containerAnimation: tlMain,
-      start: "left left",
-      end: "right left",
-      scrub: true
-    }
-  }).fromTo(".img-wrap.is-hero",
-  	{ xPercent: -33.33, transform: 'translate3d(0,0,0)', },
-  	{ xPercent: 15, transform: 'translate3d(0,0,0)', ease: "none" }
-  );
-  
+    .timeline({
+      scrollTrigger: {
+        trigger: ".empty-panel",
+        containerAnimation: tlMain,
+        start: "left left",
+        end: "right left",
+        scrub: true
+      }
+    }).fromTo(".img-wrap.is-hero",
+      { xPercent: -33.33, transform: 'translate3d(0,0,0)', },
+      { xPercent: 15, transform: 'translate3d(0,0,0)', ease: "none" }
+    );
+
   // values panel
   gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".value-panel",
-      containerAnimation: tlMain,
-      start: "left left",
-      end: "right right",
-      scrub: true
-    }
-  })
-  .to(".value-container", { xPercent: 300, ease: "none" } )
-  .fromTo(".dutoit-history",
-  	{ yPercent: 100 },
-  	{ yPercent: -100, ease: "none" },
-    0
-  )
-  .fromTo(".old-farm-photo",
-  	{ yPercent: 350 },
-  	{ yPercent: -100, ease: "none" },
-    0
-  );
-  
-// innovation panel
+    .timeline({
+      scrollTrigger: {
+        trigger: ".value-panel",
+        containerAnimation: tlMain,
+        start: "left left",
+        end: "right right",
+        scrub: true
+      }
+    })
+    .to(".value-container", { xPercent: 300, ease: "none" })
+    .fromTo(".dutoit-history",
+      { yPercent: 100 },
+      { yPercent: -100, ease: "none" },
+      0
+    )
+    .fromTo(".old-farm-photo",
+      { yPercent: 350 },
+      { yPercent: -100, ease: "none" },
+      0
+    );
+
+  // innovation panel
   gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".clip-wrap",
-      containerAnimation: tlMain,
-      start: "left left",
-      end: "right right",
-      scrub: true
-    }
-  })
-  .to(".clip-content", { xPercent: 300, ease: "none" })
-  .fromTo(
-    ".trust-container",
-    { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
-    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
-    0
-  );
-  
-  // community
+    .timeline({
+      scrollTrigger: {
+        trigger: ".clip-wrap",
+        containerAnimation: tlMain,
+        start: "left left",
+        end: "right right",
+        scrub: true
+      }
+    })
+    .to(".clip-content", { xPercent: 300, ease: "none" })
+    .fromTo(
+      ".trust-container",
+      { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
+      { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
+      0
+    );
+}
+
+// community
+function communityClipAnimation() {
   gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".temp.is-01",
-      start: "top bottom",
-      end: "bottom top",
-      scrub: true
-    }
-  }).fromTo(
-    ".community-seg.is-02",
-    { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
-    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
-    0
-  );
-  
-	gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".temp.is-02",
-      start: "bottom bottom",
-      end: "bottom top",
-      scrub: true
-    }
-  }).fromTo(
-    ".community-seg.is-03",
-    { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
-    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
-    0
-  );
-  
-	gsap
-  .timeline({
-    scrollTrigger: {
-      trigger: ".temp.is-03",
-      start: "bottom bottom",
-      end: "bottom top",
-      scrub: true
-    }
-  }).fromTo(
-    ".community-seg.is-04",
-    { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
-    { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
-    0
-  );
+    .timeline({
+      scrollTrigger: {
+        trigger: ".temp.is-01",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: true
+      }
+    }).fromTo(
+      ".community-seg.is-02",
+      { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
+      { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
+      0
+    );
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".temp.is-02",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: true
+      }
+    }).fromTo(
+      ".community-seg.is-03",
+      { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
+      { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
+      0
+    );
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".temp.is-03",
+        start: "bottom bottom",
+        end: "bottom top",
+        scrub: true
+      }
+    }).fromTo(
+      ".community-seg.is-04",
+      { clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)" },
+      { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
+      0
+    );
+}
+
 
 function familyClipPathAnimation() {
   const elem = document.getElementById('hover-box');
-	
+
   // Attach mousemove event 
-  elem.addEventListener('mousemove', function(event){
+  elem.addEventListener('mousemove', function (event) {
 
-     // Get the target
-     const target = event.target;
+    // Get the target
+    const target = event.target;
 
-     // Get the bounding client
-     const rect = target.getBoundingClientRect();
+    // Get the bounding client
+    const rect = target.getBoundingClientRect();
 
-     // Get X & Y coordinates in percentage
-     let xPos = ((event.clientX - rect.left) / rect.width) * 100;
-     let yPos = ((event.clientY - rect.top) / rect.height) * 100;
-	
-	// percentage  
-     var pos = `${xPos}% ${yPos}%`;
-     
-     // foreground pattern element
-     var foreground = document.getElementById("clip-foreground")
-     
-     // Change the clip-path position
-     foreground.setAttribute("style", `clip-path: circle(10rem at ${pos});`)
+    // Get X & Y coordinates in percentage
+    let xPos = ((event.clientX - rect.left) / rect.width) * 100;
+    let yPos = ((event.clientY - rect.top) / rect.height) * 100;
+
+    // percentage  
+    var pos = `${xPos}% ${yPos}%`;
+
+    // foreground pattern element
+    var foreground = document.getElementById("clip-foreground")
+
+    // Change the clip-path position
+    foreground.setAttribute("style", `clip-path: circle(10rem at ${pos});`)
   });
 }
 
